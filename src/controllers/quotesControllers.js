a6546e8d3b9ca30f8087b290b16d67a86bdc63b5
motajessica <motajessicaa@gmail.com>
2023-05-15 11:19:21 +1200

    Update the function to calculate the car value

--- src/controllers/quotesControllers.js
+++ src/controllers/quotesControllers.js
@@ -1,11 +1,17 @@
 import * as quoteService from '../services/quoteService.js'
+import { calculateCarValue } from '../services/carValueService';
 
-export const calculateCarValue = (req, res) => {
-  const {model, year} = req.params 
+export const calculateCarValueController = (req, res) => {
+  const { model, year } = req.body;
 
-  const result = quoteService.calculateCarValue(model, year);
-  // implement response 
-}
+  if (!model || !year || typeof model !== 'string' || typeof year !== 'number') {
+    return res.status(400).json({ error: 'Invalid input' });
+  }
+
+  const carValue = calculateCarValue(model, year);
+
+  res.json({ car_value: carValue });
+};
 
 export const calculateRiskRating  = (req, res) => {
   
@@ -14,25 +20,3 @@ export const calculateRiskRating  = (req, res) => {
 export const calculateQuote = (req, res) => {
   
 }
-
-// Just leaving it here for response implementation reference. 
-// export const createOneTask = (req, res) => {
-//   const name = req.body.name
-//   const description = req.body.description
-
-//   const newTask = taskService.createOneTask(name, description)
-
-//   res.send(newTask)
-// }
-
-// export const getOneTask = (req, res) => {
-//   const taskId = parseInt(req.params.id)
-
-//   try {
-//     const matchedTask = taskService.getOneTask(taskId)
-//     res.send(matchedTask)
-//   } catch (e) {
-//     res.status(404).send(e)
-//   }
-// }
-
--- src/services/quoteService.js
+++ src/services/quoteService.js
@@ -1,11 +1,18 @@
 export const calculateCarValue = (model, year) => {
-  //implement calculation 
-}
+  const sanitizedModel = model.replace(/[^a-zA-Z]/g, '').toUpperCase();
+  let carValue = 0;
+  for (let i = 0; i < sanitizedModel.length; i++) {
+    const charCode = sanitizedModel.charCodeAt(i);
+    carValue += charCode - 64; 
+  }
+  carValue = (carValue * 100) + year;
+  return carValue;
+};
+
 
 export const calculateRiskRating = () => {
 
 }
-
 
 export const calculateQuote  = () => {
   
