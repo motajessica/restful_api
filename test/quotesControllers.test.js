2db942080d91fb988514f12e6a489601450c728e
motajessica <motajessicaa@gmail.com>
2023-05-15 15:32:15 +1200

    Add a test case scenario

--- test/quotesControllers.test.js
+++ test/quotesControllers.test.js
@@ -7,7 +7,6 @@ import { calculateCarValue } from '../src/controllers/quotesControllers.js';
 describe('calculateCarValue', () => {
   it('should return the correct car value for the best scenario', () => {
     const req = { body: { model: 'Civic', year: 2014 } };
-
     const res = {
       json: function (data) {
         expect(data).to.have.property('car_value').to.equal(6614);
@@ -17,38 +16,21 @@ describe('calculateCarValue', () => {
     calculateCarValue(req, res);
   });
 
-  // Add more test cases as needed
+  it('sshould handle only numbers for model and return the correct car value', () => {
+    const req = { body: { model: '911', year: 2020 } };
+    const res = {
+      json: function (data) {
+        expect(data).to.have.property('car_value').to.equal(2020);
+      }
+    };
+
+    calculateCarValue(req, res);
+  });
+
 });
 
-// describe('calculateCarValue', () => {
-//   //happy paths
-//   it('return the correct car value for the best scenario', () => {
-//     const req = { body: { model: 'Civic', year: 2020 } };
-//     const res = { json: jest.fn() };
 
-//     calculateCarValue(req, res);
 
-//     expect(res.json).toHaveBeenCalledWith({ car_value: 6600 });
-//   });
-
-//   it('should handle only numbers for model and return the correct car value', () => {
-//     const req = {body: {model: 911, year: 2020 } };
-//     const res = { json: jest.fn() };
-  
-//     calculateCarValue(req, res);
-  
-//     expect(res.json).toHaveBeenCalledWith({ car_value: 2020 });
-
-//   });
-
-  // it('the minimum year accepted is 1970', () => {
-  //   const req = { body: { model: 'Accord', year: 0 } };
-  //   const res = { json: jest.fn() };
-
-  //   calculateCarValueController(req, res);
-
-  //   expect(res.json).toHaveBeenCalledWith({ car_value: {} })
-  // });
 
   //error scenarions 
 
