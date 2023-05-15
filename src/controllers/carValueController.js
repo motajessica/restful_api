0390d8a42155ee2953e556ff026437571e048fc5
motajessica <motajessicaa@gmail.com>
2023-05-16 11:02:10 +1200

    Split API intro 3

--- src/controllers/carValueController.js
+++ src/controllers/carValueController.js
@@ -1,13 +1,3 @@
-import * as carValueService from '../services/carValueService.js';
+export const calculateCarValue = (req, res) => {  
 
-export const calculateCarValue = (req, res) => {
-  const { model, year } = req.body;
-  console.log(model, year)
-
-  const serviceResponse = carValueService.calculateCarValue(model, year);
-  if (serviceResponse.error) {
-    return res.json({ error: serviceResponse.error });
-  } else {
-    return res.json({ car_value: serviceResponse.car_value });
-  }
 }
