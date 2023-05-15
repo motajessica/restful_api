e74d0baa34362e2c1189c22a59dba9faad21c322
motajessica <motajessicaa@gmail.com>
2023-05-15 20:14:31 +1200

    Add new tests cases for car value

--- test/quotesControllers.test.js
+++ test/quotesControllers.test.js
@@ -3,6 +3,7 @@ import { expect } from 'chai'
 import spies from 'chai-spies';
 chai.use(spies);
 import { calculateCarValue } from '../src/controllers/quotesControllers.js';
+import { error } from 'console';
 
 describe('calculateCarValue', () => {
   it('should return the correct car value for the best scenario', () => {
@@ -12,52 +13,47 @@ describe('calculateCarValue', () => {
         expect(data).to.have.property('car_value').to.equal(6614);
       }
     };
-
     calculateCarValue(req, res);
   });
 
-  it('sshould handle only numbers for model and return the correct car value', () => {
+  it('should handle only numbers for model and return the correct car value', () => {
     const req = { body: { model: '911', year: 2020 } };
     const res = {
       json: function (data) {
         expect(data).to.have.property('car_value').to.equal(2020);
       }
     };
-
     calculateCarValue(req, res);
   });
 
+
+  it('negative year scenario', () => {
+    const req = { body: { model: 'Prius', year: -2018 } };
+    const res = {
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function(data) {
+        expect(this.statusCode).to.equal(400);
+        expect(data).to.deep.equal({ error: 'Invalid input' });
+      }
+    };
+    calculateCarValue(req, res);
+  });
+
+  it('empty year scenario', () => {
+    const req = { body: { model: 'C200', year: ""} };
+    const res = {
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function(data) {
+        expect(this.statusCode).to.equal(400);
+        expect(data).to.deep.equal({ error: 'Invalid input' });
+      }
+    };
+    calculateCarValue(req, res);
+  });
 });
-
-
-
-
-  //error scenarions 
-
-  // it('should return an error message for negative year scenario', () => {
-    
-  // });
-
-  // it('should return an error message for empty year scenario', () => {
-    
-  // });
-
-  // it('should return an error message for empty year scenario', () => {
-    
-  // });
-
-  // it('Should return an error message for empty model name scenario', () => {
-
-  // })
-
-  // it('Should return an error message for year in future scenario', () => {
-
-  // })
-
-  // it('Should return an error message for year provided as a string instead of a number', () => {
-
-  // })
-
-
-  
-// });
