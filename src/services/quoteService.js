744e9843df66f2fab5e5ec931916d51d6e44e192
Rainbow <kenesou@gmail.com>
2023-05-16 15:53:40 +1200

    update the Jest set up with typescript and update one unit testin ts

--- src/services/quoteService.js
+++ src/services/quoteService.js
@@ -1,22 +1,13 @@
-
-export const calculateQuote = (carValue, riskRating) => {
-  if (
-    typeof riskRating !== "number" ||
-    typeof carValue !== "number" ||
-    riskRating < 1 ||
-    riskRating > 5
-  ) {
-    throw new Error("This is an error.");
-  }
-
-  const yearlyPremium = Math.floor((carValue * riskRating) / 100);
-  const monthPremium = yearlyPremium / 12;
-
-  const output = {
-    yearlyPremium: yearlyPremium,
-    monthPremium: monthPremium,
-  };
-
-  return output;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculateQuote = void 0;
+const calculateQuote = (carValue, riskRating) => {
+    const yearlyPremium = Math.floor((carValue * riskRating) / 100);
+    const monthPremium = yearlyPremium / 12;
+    const output = {
+        monthPremium: monthPremium,
+        yearlyPremium: yearlyPremium,
+    };
+    return output;
 };
-
+exports.calculateQuote = calculateQuote;
--- src/services/quoteService.ts
+++ src/services/quoteService.ts
@@ -1,12 +1,16 @@
-export const calculateQuote = (carValue:number, riskRating:number) => {
-  
-    const yearlyPremium = Math.floor((carValue * riskRating) / 100);
-    const monthPremium = yearlyPremium / 12;
-  
-    const output = {
-      yearlyPremium: yearlyPremium,
-      monthPremium: monthPremium,
-    };
-  
-    return output;
+interface Quote {
+  yearlyPremium: number;
+  monthPremium: number;
+}
+
+export const calculateQuote = (carValue: number, riskRating: number): Quote => {
+  const yearlyPremium = Math.floor((carValue * riskRating) / 100);
+  const monthPremium = yearlyPremium / 12;
+
+  const output: Quote = {
+    monthPremium: monthPremium,
+    yearlyPremium: yearlyPremium,
   };
+
+  return output;
+};
