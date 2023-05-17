1a9232407a1e06194b2ba67c7260abdc55bcccdf
Rainbow <kenesou@gmail.com>
2023-05-17 21:35:49 +1200

    update the void in the controller function

--- src/controllers/quotesControllers.ts
+++ src/controllers/quotesControllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import * as quoteService from '../services/quoteService';
 import { Quote } from "../types/Quote";
 
-export const calculateQuote = (req: Request, res: Response) => {
+export const calculateQuote = (req: Request, res: Response):void => {
   try {
     const carValue = req.body.carValue;
     const riskRating = req.body.riskRating;
