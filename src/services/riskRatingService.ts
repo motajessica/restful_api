44131e5a4e798789f00dcb8a3a8331b1d4206a16
Ricardo <diiaz2910@gmail.com>
2023-05-18 15:57:44 +1200

    risk rating code update

--- src/services/riskRatingService.ts
+++ src/services/riskRatingService.ts
@@ -1,3 +1,16 @@
-export const calculateRiskRating = () => {
+export function calculateRiskRating(input: { claim_history: string }): { risk_rating: number } | Error {
+    const keywordList = ["collide", "crash", "scratch", "bump", "smash"];
 
+    if (!input || !input.claim_history || typeof input.claim_history !== 'string') {
+        return new Error('Invalid input');
+    }
+    const claimHistory = input.claim_history.toLowerCase();
+    let riskRating = 0;
+
+    keywordList.forEach(keyword => {
+        const occurrences = claimHistory.split(keyword).length - 1;
+        riskRating += occurrences;
+    });
+
+    return { risk_rating: riskRating };
 }
